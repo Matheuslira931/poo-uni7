a333da7c745daaf2fe241334360ded80d5f1e36d
Matheus <matheus.lira93@gmail.com>
2021-02-22 19:54:44 -0300

    Revert "Revert "Atividade aula 10.02 carro""
    This reverts commit 8d8c86e7d09f065d57adbb884689ca0df5d54416.

---
+++ att-10.02-carro/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+  public static void main(String[] args) {
+    Carro car1 = new Carro();
+    car1.marca = "honda";
+    car1.modelo = "civic";
+    car1.tanque = 1;
+
+    car1.andar(16);
+  }
+  
+}
