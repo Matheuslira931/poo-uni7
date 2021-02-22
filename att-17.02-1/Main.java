0b8446f21c462f66b10e226d20ab3ce91034019c
Matheus <matheus.lira93@gmail.com>
2021-02-22 19:47:41 -0300

    [ignorar esse commit, testes]

---
+++ att-17.02-1/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Data data = new Data(22, 12, 1999);
+        System.out.println(data.getDia());
+        System.out.println(data.getMes());
+        System.out.println(data.getAno());
+    }
+}
--- newFile.java
+++ newFile.java
@@ -1,3 +1,9 @@
-public static void main (String arg []){
-	System.out.println("hello world");
+/**
+ * newFile
+ */
+public class newFile {
+
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo!");
+	}
 }
